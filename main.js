6accdaa7d451406eff295d2483be14d283a98088
ridasersif <ridasersif1@gmail.com>
2024-11-12 14:13:50 +0100

    Ajout de la fonction d'impression des tickets de voyage

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let step2=document.getElementById('step-2');
 let step3=document.getElementById('step-3');
 let step4=document.getElementById('step-4');
 let step5=document.getElementById('step-5');
-
 //*******************************************/
 let tab=document.getElementsByClassName('tab')
 //******************************************/
@@ -16,7 +15,6 @@ let btnAdultBlus=document.getElementById('btnAdultBlus')
 let btnAdultMoins=document.getElementById('btnAdultMoins')
 let btnEnfantBlus=document.getElementById('btnEnfantBlus')
 let btnEnfantMoins=document.getElementById('btnEnfantMoins')
-
 let next4=document.getElementById('next4')
 
 let currentPosition = 0;
@@ -29,7 +27,6 @@ function next()
     tab[3].style.zIndex='0'
     tab[4].style.zIndex='0' 
     currentPosition++;
-  
     let currentStep = document.getElementById('step-'+(currentPosition+1))
     currentStep.click();
     tab[currentPosition].style.zIndex='100'
@@ -45,13 +42,11 @@ function back(){
     let currentStep = document.getElementById('step-'+(currentPosition+1))
     currentStep.click();
     tab[currentPosition].style.zIndex='100'
-
 }
-
-
-
 tab[0].style.zIndex='100'
 /******************************************* */
+
+
  
   const nextButton=document.getElementById('nextStep1');
    nextButton.style.backgroundColor = "#ccc"
@@ -66,6 +61,10 @@ tab[0].style.zIndex='100'
   }
   nom.addEventListener("input",checkInputs)
   email.addEventListener("input",checkInputs)
+
+
+
+
 /****************************************************************************** */
 step2.onclick=function(){
 const depart = document.getElementById("depart");
@@ -87,6 +86,8 @@ arriver.addEventListener("select", checkInputs);
 date.addEventListener("input", checkInputs);
 }
 /********************************************************************************************/
+var deparTime;
+var arrivTime;
 step3.onclick=function(){
   const resultDivs = document.querySelectorAll(".ResultatDeDecherche");
   const nextButton = document.getElementById("nextStep3");
@@ -101,9 +102,13 @@ step3.onclick=function(){
       resultDivs.forEach(Div => {
         if (Div !== div) {
           Div.style.backgroundColor = ""; 
-          Div.style.color = "";  
+          Div.style.color = ""; 
+
         }
       });
+       deparTime = div.querySelector("#deparTime").innerText;
+       arrivTime = div.querySelector("#arrivTime").innerText;
+      console.log(deparTime,arrivTime)
     });
   });
 }
@@ -121,7 +126,6 @@ step4.onclick=function(){
 let PrixTotal=document.getElementById('PrixTotal');
 const  nextButton =document.getElementById('nextStep4');
 nextButton.style.backgroundColor = "#ccc"
-
 btnAdultBlus.onclick=function(){
   if((i+j)<16){
     x=x+500;
@@ -137,8 +141,6 @@ btnAdultBlus.onclick=function(){
   }
     let total=Total(x,y)
     PrixTotal.innerHTML=`Prix total : ${total} DH`
-   
-   
 }
 btnAdultMoins.onclick=function(){
   if(i>0){
@@ -155,10 +157,7 @@ document.getElementById('NPA').innerHTML=`
   }
  let total=Total(x,y)
     PrixTotal.innerHTML=`Prix total : ${total} DH`
-    
-   
 }
-
 btnEnfantBlus.onclick=function(){
   if((i+j)<16){
     y=y+100;
@@ -172,12 +171,8 @@ document.getElementById('NPE').innerHTML=`
            ${j}
 `
   }
-
  let total=Total(x,y)
     PrixTotal.innerHTML=`Prix total : ${total} DH`
-    
-    
-   
 }
 btnEnfantMoins.onclick=function(){
   if(j>0){
@@ -192,7 +187,6 @@ document.getElementById('NPE').innerHTML=`
            ${j}
 `
   }
- 
  let total=Total(x,y)
     PrixTotal.innerHTML=`Prix total : ${total} DH`  
 }
@@ -205,7 +199,6 @@ function checkNextButton() {
     nextButton.style.backgroundColor = "#ccc"; 
   }
 }
-
 }
 let c=2;
 let buttonPrix=document.getElementById('buttonPrix');
@@ -224,23 +217,27 @@ function inputCheckbox(){
   c++;
  
 }
-
 step5.onclick=function(){
     let ticket=document.getElementById('ticket')
-    console.log(i,j)
-    let total=Total(x,j)
-    let ij=0;
-    for(let m=0;m<(i+j);m++){
+   
+    var ij=0;
+    if(i!=0){
+    for(let m=0;m<i;m++){
       ij++;
+      const depart=document.getElementById('depart')
+      const arriver=document.getElementById('arriver')
         ticket.innerHTML+=`
         <div class="card content">
         <div  class="user-info">
         <p>(${ij})</p>
         <p>Nom et prénom : <span>${nom.value}</span></p>
-                  <p>E-mail : <span>${email.value}</span></p>
-                  <p>Date de réservation : <span>${date.value}</span></p>
-                  <p>Prix : <span> ${total} DH</span></p>
-        
+        <p>E-mail : <span>${email.value}</span></p>
+          <hr>
+        <p><span>${depart.value} :</span><span> ${deparTime} </span></p>
+        <i class="fa-solid fa-arrow-down-long"></i>
+        <p><span>${arriver.value} :</span><span> ${arrivTime}</span></p>
+        <p>Date de réservation : <span>${date.value}</span></p>
+        <p>Prix : <span> 500 DH</span></p>
         </div>
         <div class="qr-code">
           <p>Code QR :</p>
@@ -248,10 +245,65 @@ step5.onclick=function(){
         </div>
       </div>`
         
+    }  
     }
-               
+    if(j!=0){
+      for(let m=0;m<j;m++){
+        ij++;
+        const depart=document.getElementById('depart')
+        const arriver=document.getElementById('arriver')
+          ticket.innerHTML+=`
+          <div class="card content">
+          <div  class="user-info">
+          <p>(${ij})</p>
+          <p>Nom et prénom : <span>${nom.value}</span></p>
+          <p>E-mail : <span>${email.value}</span></p>
+            <hr>
+          <p><span>${depart.value} :</span><span> 06:30 </span></p>
+          <i class="fa-solid fa-arrow-down-long"></i>
+          <p><span>${arriver.value} :</span><span> 08:45</span></p>
+          <p>Date de réservation : <span>${date.value}</span></p>
+          <p>Prix : <span> 100 DH</span></p>
+          </div>
+          <div class="qr-code">
+            <p>Code QR :</p>
+            <img src="qr-code.svg" alt="code QR"/>
+          </div>
+        </div>`
+          
+      }  
+    }
+              
 }
+
+function generatePDF() {
+  for(let q=0;q<5;q++){
+  const ticketElement = document.getElementById('ticket');
+  html2canvas(ticketElement, { scale: 2 }).then(canvas => {
+    const imgData = canvas.toDataURL('image/png');
+    const pdf = new jspdf.jsPDF('p', 'mm', 'a4');
+    const imgWidth = 190;  
+    const pageHeight = pdf.internal.pageSize.height;
+    const imgHeight = canvas.height * imgWidth / canvas.width;
+    let position = 0;
+    pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+    pdf.save('tickets.pdf'); 
+  });
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //******************************************************** */
-
-
-
